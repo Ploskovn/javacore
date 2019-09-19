487faa1190ae3de7e04c6b7cdae18ffd341a221e
ploskovn <ploskovn@mail.ru>
2019-09-19 22:38:18 +0500

    Example

---
+++ src/main/java/com/ploskovn/javacore/chapter02/Example.java
@@ -0,0 +1,10 @@
+/*
+Это простая программа на Java.
+Присвоить исходному файлу имя "Example.java"
+*/
+class Example {
+	//Это программа начинается с вызова метода main()
+	public static void main(String args[]) {
+		System.out.println("Простая программа на Java.");
+	}
+}
