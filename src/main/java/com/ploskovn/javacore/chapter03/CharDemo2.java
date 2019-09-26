140a31442d7c635a063e2d12c6920dec5b37937d
ploskovn <ploskovn@mail.ru>
2019-09-26 23:20:02 +0500

    CharDemo2

---
+++ src/main/java/com/ploskovn/javacore/chapter03/CharDemo2.java
@@ -0,0 +1,13 @@
+// Символьные переменные ведут себя как целочисленные значения
+class CharDemo2 {
+	
+	public static void main(String args[] ) {
+		char chl;
+		
+		chl = 'X';
+		System.out.println("ch1 содержит " + chl) ;
+		
+		chl++; // увеличить на единицу значение переменной ch1
+		System.out.println("chl теперь содержит " + chl);
+	}
+}
