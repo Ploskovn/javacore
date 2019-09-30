4d9d21ab7345953883c1db2218b57593fa2bf0f1
ploskovn <ploskovn@mail.ru>
2019-09-30 23:51:39 +0500

    chapter 3,4,5

---
+++ src/main/java/com/ploskovn/javacore/chapter05/NoBody.java
@@ -0,0 +1,11 @@
+class NoBody {
+    public static void main(String args[]) {
+        int i, j;
+
+        i = 100;
+        j = 200;
+
+        while (++i < --j) ; // у этого цикла отсутствует тело
+        System.out.println("Среднее значение равно " + i);
+    }
+}
